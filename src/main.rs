7a1fff6abfdc946c4bc88c997050e3c16a383710
officeyutong <9004058+Officeyutong@users.noreply.github.com>
2023-02-14 21:52:18 +0800

    remove unwrap

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ fn main() {
     let mut config = wasmtime::Config::new();
     config.wasm_component_model(true);
     let engine = wasmtime::Engine::new(&config).unwrap();
-    wasmtime::component::Component::from_file(&engine, "./rust-bootstrap-component.wasm").unwrap();
+    wasmtime::component::Component::from_file(&engine, "./rust-bootstrap-component.wasm").ok();
 }
